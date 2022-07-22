200b9a376de7a9b0bd7a2aaa0f608ad4ef44b72b
Ninja <ninja@Andress-MacBook-Pro.local>
2022-07-22 17:22:01 -0500

    ultimo commit de leccion-10 y final de clase :)

--- README.md
+++ README.md
@@ -6,6 +6,14 @@
 * añadir métodos login() y logout() a protipo de Usuario
 * invokar método login() y logout() de Usuario 
 * usar ..args (arreglo de parámetros para una función
+* usar apply() pasando 'args' para que el prototipo Admin herede las propiedades del prototipo Usuario (email y nombre)
+* usar Object.create() para que Admin herede los métodos de Usuario (login() y logout()) (cadena de prototipos)
+* crear dos objetos de tipo Usuario
+* crear un objeto administrador de tipp Admin
+* invocar método login() y logout de usuario/administrador
+* crear una cadena de usuarios con ususarios y administrador
+* administrador puede eliminar un usuario
+* el usuario normal no puede eliminar un usuario porque eliminanUsuario() no existe en su prototipo
 
 
 **Link al curso traducido en YouTube:** [JavaScript Moderno](https://www.youtube.com/channel/UCuSHTq2yiCY5QBNoEXv8JpA/)
--- objeto.js
+++ objeto.js
@@ -16,19 +16,46 @@ Usuario.prototype.logout = function() {
     console.log(this.email, "ha cerrado su sesión");
 }
 
+// usar apply() pasando 'args' para que el prototipo Admin herede las propiedades del prototipo Usuario (email y nombre)
 function Admin(...args) {
-    Usuario.apply(this, args);
+    Usuario.apply(this, args);   
+    this.permiso = 'super admin';
+}
+
+// usar Object.create() para que Admin herede los métodos de Usuario (login() y logout()) (cadena de prototipos)
+Admin.prototype = Object.create(Usuario.prototype);  
+
+Admin.prototype.eliminarUsuario = function(u) {
+    usuarios = usuarios.filter(user => {
+        return user.email != u.email;
+    });
 }
 
 // crear dos objetos de tipo Usuario
 let primerUsuario = new Usuario('ryu@ninjasdelcodigo.pe', 'Ryu');
 let segundoUsuario = new Usuario('yoshi@ninjasdelcodigo.pe', 'Yoshi');
-console.log(primerUsuario);
-console.log(segundoUsuario);
 
-// crear un administrador
+// crear un objeto administrador de tipp Admin
 let administrador = new Admin('shaun@ninjasdelcodigo.pe', 'Shaun');
 
-// invokar método login() de usuario
+console.log(primerUsuario);
+console.log(segundoUsuario);
+console.log(administrador);
+
+// invocar método login() y logout de usuario/administrador
 primerUsuario.login();
 segundoUsuario.login();
+administrador.login();
+administrador.logout();
+segundoUsuario.logout();
+
+// crear una cadena de usuarios con ususarios y administrador
+let usuarios = [primerUsuario, segundoUsuario, administrador];
+console.log(usuarios);
+
+// administrador puede eliminar un usuario
+administrador.eliminarUsuario(segundoUsuario);
+console.log(usuarios);
+
+// el usuario normal no puede eliminar un usuario porque eliminanUsuario() no existe en su prototipo
+// primerUsuario.eliminarUsuario(administrador);
